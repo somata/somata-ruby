ed58ee970e9807bf768c9e879093c26f3b27e6ca
Sean Robertson <sprobertson@gmail.com>
2014-10-15 17:44:49 -0700

    a more meta way to determine what handler to use; handlign subscribe and unsubscribe messages

--- service.rb
+++ service.rb
@@ -22,6 +22,8 @@ class Service
         @poller.register_readable(@socket)
         @http = Net::HTTP.new('localhost', 8500)
 
+        @subscriptions = {}
+
         self.register
         Thread.new { self.pass_checks_loop }
     end
@@ -58,15 +60,13 @@ class Service
     end
 
     def handle_message(sock, client_id, message)
-
-        if message['kind'] == 'method'
-            self.handle_method(sock, client_id, message)
-
+        handler_name = 'handle_' + message['kind']
+        if self.respond_to?(handler_name)
+            self.send(handler_name, sock, client_id, message)
         else
             # TODO: Handle other message kinds
             puts "Unrecognized message: #{ message }"
         end
-
     end
 
     # Handlers
@@ -92,6 +92,45 @@ class Service
             puts @methods
         end
 
+    end
+
+    def handle_subscribe(sock, client_id, message)
+        puts "Subscribing: " + message.to_json
+        event_type = message['type']
+
+        new_subscription = {
+            'id' => message['id'],
+            'client_id' => client_id
+        }
+
+        if subscriptions = @subscriptions[event_type]
+            subscriptions.push new_subscription
+        else
+            @subscriptions[event_type] = [new_subscription]
+        end
+
+        puts "Subscriptions: " + @subscriptions.to_json
+    end
+
+    def handle_unsubscribe(sock, client_id, message)
+        puts "Subscribing: " + message.to_json
+        event_type = message['type']
+
+        if subscriptions = @subscriptions[event_type]
+            subscriptions.delete_if { |s| s['id'] == message['id'] }
+        end
+        puts "Subscriptions: " + @subscriptions.to_json
+    end
+
+    # Event emitting
+    # --------------------------------------------------------------------------
+
+    def emit(event_type, data)
+        if subscriptions = @subscriptions[event_type]
+            for subscription in subscriptions
+                puts "Subscribed: " + subscription.to_json
+            end
+        end
     end
 
     # Service registration and health checking
